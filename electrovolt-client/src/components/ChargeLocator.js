f40e71eb13d329115859ce133dbe96132bda04d9
Hetvi <98730578+hetvishahh2@users.noreply.github.com>
2023-12-24 15:26:37 +0530

    Update ChargeLocator.js

--- electrovolt-client/src/components/ChargeLocator.js
+++ electrovolt-client/src/components/ChargeLocator.js
@@ -1,5 +1,4 @@
-// ChargeLocator.js
-// ChargeLocator.js
+
 import React,{useEffect} from 'react';
 import './ChargeLocator.css'; // Import the corresponding CSS file for styling
 import AOS from 'aos';
@@ -18,48 +17,37 @@ const ChargeLocator = () => {
     // <div>
        
     <div className="charge-locator-container">
-       
-      <div className="search-bar">
-      
-        <div className="text-box-container">
-        <h2 className="fonts" data-aos="fade-up">Charger Locator </h2>
-            <div className="text-box">
             
-                <label htmlFor="textBox1">Search:</label>
-                <input type="text" id="textBox1" name="textBox1" />
+            <div className="map-container">
+              <iframe
+                title="Map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3520346.028877583!2d74.20073982374853!3d20.338006073976866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7ce7dadc6d62d%3A0x8fbc4ba51b1ad7e7!2sIndia!5e0!3m2!1sen!2sus!4v1638255055004!5m2!1sen!2sus"
+                width="100%"
+                height="130%"
+                frameBorder="0"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+              ></iframe>
             </div>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <h2 className="fonts1" data-aos="fade-up">Trip Planner </h2>
-            <div className="text-box">
-                <label htmlFor="textBox2">From:</label>
-                <input type="text" id="textBox2" name="textBox2" />
-            </div>
+            <div className="contact-form1" data-aos="fade-left">
+              <br/>
+                <h2>Charger Locator</h2>
+                <form autoComplete="off">
+                  <label htmlFor="name">Search:</label>
+                  <input type="text" id="name" name="name" autoComplete="off" />
+                </form>
+                <h2>Trip Planner </h2>
+                <form>  
+                  <label htmlFor="email">From:</label>
+                  <input type="email" id="email" name="email" autoComplete="off"/>
 
-            <div className="text-box">
-                <label htmlFor="textBox3">To:</label>
-                <input type="text" id="textBox3" name="textBox3" />
-            </div>
-      </div>
 
-      <div className="map-container">
-        <iframe
-          title="Map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3520346.028877583!2d74.20073982374853!3d20.338006073976866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7ce7dadc6d62d%3A0x8fbc4ba51b1ad7e7!2sIndia!5e0!3m2!1sen!2sus!4v1638255055004!5m2!1sen!2sus"
-          width="92%"
-          height="100%"
-          frameBorder="0"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          loading="lazy"
-        ></iframe>
-      </div>
-    </div>
-     
-       
+                  <label htmlFor="phone">To:</label>
+                  <input type="tel" id="phone" name="phone"  autoComplete="off"/>
+
+                </form>
+            </div>
     </div>
     
    
