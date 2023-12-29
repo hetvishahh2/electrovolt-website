74c6d249a4385b430af3db5028921768d0a665f5
Hetvi <98730578+hetvishahh2@users.noreply.github.com>
2023-12-29 21:19:23 +0530

    Update server.js

--- electrovolt-server/server.js
+++ electrovolt-server/server.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 // require('dotenv').config();
+const path = require('path');
 
 
 const app = express();
